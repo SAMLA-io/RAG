67e5853ba7969aec4a34842dc88be3681e2035b0
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-01-10 18:59:14 +0000

    Created app.py

---
+++ rag/app.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.database import router
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+ CORSMiddleware,
+ allow_origins=origins,
+ allow_credentials=True,
+ allow_methods=["*"],
+ allow_headers=["*"],
+)
+
+app.include_router(router, prefix="/api")
+
+@app.get("/")
+async def test():
+ return "Hello World!"
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
