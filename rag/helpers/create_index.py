b9e2dd2af837d86bd056b255f76558b1e88c6b33
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-01-10 18:58:55 +0000

    Created create_index function

---
+++ rag/helpers/create_index.py
@@ -0,0 +1,39 @@
+# Written by Juan Pablo Gutiérrez
+# 10/01/2025
+# This script creates a vector search index for a given database and collection
+
+import os
+import dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_mongodb import MongoDBAtlasVectorSearch
+from pymongo import MongoClient
+dotenv.load_dotenv()
+
+MONGODB_ATLAS_CLUSTER_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGODB_ATLAS_CLUSTER_URI)
+
+def create_index(database, collection, dimensions=3072):
+    index_name = f"{database}_{collection}_index"
+    collection = client[database][collection]
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    
+    vector_store = MongoDBAtlasVectorSearch(
+        collection=collection,
+        embedding=embeddings,
+        index_name=index_name,
+        relevance_score_fn="cosine",
+    )
+
+    try:
+        vector_store.create_vector_search_index(dimensions=dimensions)
+    except Exception as e:
+        return {
+            "statusCode": "500",
+            "body" : f"Error creating index: {e}"
+        }
+
+    return {
+        "statusCode": "200",
+        "body" : "Index created successfully"
+    }
